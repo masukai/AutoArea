679f85c001c4ae8aa05eedd20e83bc76513b9bd0
KaitoMasuda <37993351+masukai@users.noreply.github.com>
2020-06-06 15:28:04 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import glob
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+# _listはリスト
+# np_はnp.arrayに格納されている
+
+
+def main():  # メイン関数
+
+
+if __name__ == '__main__':
+    main()
